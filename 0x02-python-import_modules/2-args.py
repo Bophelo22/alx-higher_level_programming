d3c9153a5ceca02bdeeb91b15dce3f5dcae3c141
Bophelo22 <tshepiso9602@gmail.com>
2023-11-30 23:22:05 +0000

    2-args.py committed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    arg_len = len(sys.argv)
-    #print(arg_len)
-    if arg_len == 1:
-        args_len = 0
-        print("{} arguments.".format(args_len))
-    elif arg_len == 2:
-        args_len = 1
-        print("{} argument:".format(args_len))
-        print("{}: {}".format(args_len,sys.argv[1]))
+    argLen = len(sys.argv)
+    if argLen == 1:
+        print("{} arguments.".format(argLen - 1))
+    elif argLen == 2:
+        print("{} argument:".format(argLen - 1))
     else:
-        args_len = arg_len -1
-        print("{} arguments:".format(args_len))
-        for arguement in range(1,arg_len):
-            print("{}: {}".format(arguement, sys.argv[arguement]))
+        print("{} arguments:".format(argLen - 1))
+        for i in range(1, argLen):
+            print("{}: {}".format(i, sys.argv[i]))
