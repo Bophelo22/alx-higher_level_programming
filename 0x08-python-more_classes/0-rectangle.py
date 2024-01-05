6a10c88b05674ae39e8ba77d508d72687e07ca27
Bophelo22 <tshepiso9602@gmail.com>
2024-01-05 00:56:50 +0000

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -5,6 +5,7 @@ Write an empty class Rectangle that defines a rectangle
 
 
 class Rectangle:
-    """An empty class Square that defines a square:"""
+    """
+    An empty class Square that defines a square:
+    """
     pass
-
